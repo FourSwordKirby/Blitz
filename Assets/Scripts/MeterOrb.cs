a045ee799f7f5e7bf1e08fe0092784a863c624b0
Roger Liu <rogliu26@gmail.com>
2016-01-28 02:19:29 -0500

    Initial push of current progress with the GCS project

---
+++ Assets/Scripts/MeterOrb.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeterOrb : MonoBehaviour {
+
+    public int meterGain;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        GameObject enterObject = col.gameObject;
+        Debug.Log(enterObject);
+
+        PlayerOriginPoint playerOriginPoint = enterObject.GetComponent<PlayerOriginPoint>();
+
+        if (playerOriginPoint != null)
+        {
+            playerOriginPoint.player.gainMeter(meterGain);
+            Destroy(this.gameObject);
+        }
+    }
+}
