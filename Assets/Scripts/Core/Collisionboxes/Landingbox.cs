bc33d62821c06c94f9d2987caadc4c271c6cca41
Roger Liu <rogliu26@gmail.com>
2016-02-14 16:05:05 -0500

    added pauls placeholder idle sprites. Also fixed bug regarding the player being grounded through daniels fix of using a raycast rather than a collision box for detecting groundedness

--- Assets/Scripts/Core/Collisionboxes/Landingbox.cs
+++ Assets/Scripts/Core/Collisionboxes/Landingbox.cs
@@ -4,13 +4,23 @@ using System.Collections;
 public class Landingbox : Collisionbox {
     public Player owner;
 
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        owner.grounded = true;
-    }
+    public LayerMask floorMask;
 
-    void OnCollisionExit2D(Collision2D collision)
+    void Update()
     {
-        owner.grounded = false;
+        //Note, might just make it raycast when the player isn't grounded
+        Debug.DrawRay(this.transform.position, 0.3f * Vector3.down, Color.red);
+
+        if (Physics2D.Raycast(this.transform.position, Vector3.down, 0.3f, floorMask))
+        {
+            Debug.Log("Grounded");
+            owner.grounded = true;
+        }
+        else
+        {
+
+            Debug.Log("NOT");
+            owner.grounded = false;
+        }
     }
 }
