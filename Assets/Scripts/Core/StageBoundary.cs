e9d53dfc1815df53539d6ce148ba3332c769cf0b
Roger Liu <rogliu26@gmail.com>
2016-04-03 16:42:34 -0400

    added effect for when the player dies

--- Assets/Scripts/Core/StageBoundary.cs
+++ Assets/Scripts/Core/StageBoundary.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 public class StageBoundary : MonoBehaviour {
 
+    public GameObject deathEffectPrefab;
+
     void OnTriggerExit2D(Collider2D col)
     {
         GameObject exitObject = col.gameObject;
@@ -9,7 +11,16 @@ public class StageBoundary : MonoBehaviour {
 
         if (playerOriginPoint != null)
         {
+            float angle = Vector3.Angle(col.gameObject.transform.position, this.gameObject.transform.position);
+            playDeathEffect(col.gameObject.transform.position, 90);
             playerOriginPoint.player.Die();
         }
+    }
+
+    private void playDeathEffect(Vector2 location, float rotation, ParticleSystem playerDeathEffect = null)
+    {
+        GameObject deathEffect = Instantiate(deathEffectPrefab);
+        deathEffect.gameObject.transform.position = location;
+        deathEffect.gameObject.transform.rotation = Quaternion.EulerRotation(0, 0, -15f * rotation);
     }
 }
