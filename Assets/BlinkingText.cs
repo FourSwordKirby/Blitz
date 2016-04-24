5034a6fc29d2223061f753302f99cdad6888f504
Roger Liu <rogliu26@gmail.com>
2016-04-24 17:06:37 -0400

    have all the components needed for the title screen

---
+++ Assets/BlinkingText.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BlinkingText : MonoBehaviour {
+
+    public Text text;
+    private float lowerAlpha = 0.4f;
+    private float alphaChange = 0.005f;
+    private bool increasing;
+
+	// Update is called once per frame
+	void Update () {
+        if (increasing)
+        {
+            text.color = new Color(text.color.a, text.color.g, text.color.b, text.color.a + alphaChange);
+            if (text.color.a == 1.0f)
+            {
+                increasing = false;
+            }
+        }
+        else
+        {
+            text.color = new Color(text.color.a, text.color.g, text.color.b, text.color.a - alphaChange);
+            if (text.color.a <= lowerAlpha)
+            {
+                increasing = true;
+            }
+        }
+	}
+}
---
+++ Assets/TitleControl.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleControl : MonoBehaviour {
+
+    public bool clicked;
+	// Update is called once per frame
+	void Update () {
+        if ((Input.GetButtonDown("P1 Attack") ||
+            Input.GetButtonDown("P2 Attack")) && !clicked)
+        {
+            clicked = true;
+            StartCoroutine(this.GetComponent<changeLevel>().change("Character Select"));
+        }
+	}
+}
